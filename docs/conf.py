e6dba37fa45075e16ce69c2af47815a42eab5b00
Ingmar Schoegl <ischoegl@lsu.edu>
2020-11-28 10:25:04 -0600

    Add initial Sphinx documents

---
+++ docs/conf.py
@@ -0,0 +1,73 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import ctwrap as cw
+import datetime
+
+
+# -- Project information -----------------------------------------------------
+
+
+year = datetime.date.today().year
+project = 'ctwrap'
+author = cw.__author__
+copyright = '2018-{}, {}'.format(year-2000, author)
+
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = cw.__version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'nbsphinx',
+    'sphinx.ext.mathjax'
+]
+
+suppress_warnings = [
+    'nbsphinx.localfile',
+]
+
+autodoc_default_flags = ['members', 'no-undoc-members'] # 'show-inheritance',
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'nature' #'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. ctwrap documentation master file, created by
+   sphinx-quickstart on Sat Nov 28 09:13:11 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ctwrap's - Python Wrapper for Batch Simulations
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
