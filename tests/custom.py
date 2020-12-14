255cc5b4a0d1307e51bbe8a9d28ae9e70f3acc0b
Ingmar Schoegl <ischoegl@lsu.edu>
2020-12-14 09:38:58 -0600

    Unittest for batch simulations from custom modules

---
+++ tests/custom.py
@@ -0,0 +1,20 @@
+"""Copy of minimal.py"""
+import time
+
+
+def defaults():
+    """Returns dictionary containing default arguments"""
+    return {'sleep': 0.2}
+
+
+def run(name, sleep=.2):
+    """This method 'sleeps' for the specified duration"""
+    print('    - `minimal`: sleeping for {} seconds ...'.format(sleep))
+    time.sleep(sleep)
+    return {name: {'sleep': [sleep]}}
+
+
+if __name__ == "__main__":
+    """ Main function """
+    config = defaults()
+    out = run('main', **config)
