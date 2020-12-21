2e236870b825b7ad92bfd413e4d3076c5b5a3506
Ingmar Schoegl <ischoegl@lsu.edu>
2020-12-20 19:19:33 -0600

    Bump version to 0.3.0-alpha

--- ctwrap/_version.py
+++ ctwrap/_version.py
@@ -3,7 +3,7 @@ https://setuptools.readthedocs.io/en/latest/setuptools.html#specifying-your-proj
 """
 
 __author__ = "I. Schoegl and D. Akinpelu"
-__version_info__ = (0, 2, 1)
+__version_info__ = (0, 3, 0, '-alpha')
 __version__ = '.'.join(map(str, __version_info__[:3]))
 if len(__version_info__) == 4:
     __version__ += __version_info__[-1]
